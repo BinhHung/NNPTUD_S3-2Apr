0ba6e95d7895f942c5d747c067a46a5d087ca86c
BinhHung <binhhung2399@gmail.com>
2024-04-02 21:16:09 +0700

    - Chạy được phần gửi email, - thực hiện validaton ở phần ForgotPassword (isEmail) và ResetPassword(isStrongPassword) - thực hiện chức năng ChangePassword ( yêu cầu, check đăng nhập và kiểm tra strongpassword)

---
+++ configs/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SECRET_KEY: 'TUNG123',
+    EXPIRE_JWT: '1d',
+    hostName: 'localhost:3000',
+    Host: "sandbox.smtp.mailtrap.io",
+    Port: 2525,
+    Username: "2aa5299bc599d7",
+    Password: "dc7d65460af7e5"
+}
